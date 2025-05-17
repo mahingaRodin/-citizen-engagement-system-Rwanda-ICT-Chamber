625a5aebc21bba80ca33df353a6625281296637d
P-Odiix <mahingarodin@gmail.com>
2025-05-17 15:20:32 +0200

    "[changes]: added the authentication logic"

--- engagement-system-be/src/main/java/com/citizen/engagement_system_be/serviceImpl/UserServiceImpl.java
+++ engagement-system-be/src/main/java/com/citizen/engagement_system_be/serviceImpl/UserServiceImpl.java
@@ -1,11 +1,18 @@
 package com.citizen.engagement_system_be.serviceImpl;
 
-import com.citizen.engagement_system_be.enums.NotificationType;
+import com.citizen.engagement_system_be.enums.UserRole;
+import com.citizen.engagement_system_be.exceptions.ResourceNotFoundException;
+import com.citizen.engagement_system_be.models.User;
 import com.citizen.engagement_system_be.repository.UserRepository;
 import com.citizen.engagement_system_be.services.UserService;
+import jakarta.persistence.EntityExistsException;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -17,5 +24,41 @@ public class UserServiceImpl implements UserService {
 
     public UserDetailsService userDetailsService() {
         return username -> (org.springframework.security.core.userdetails.UserDetails) userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    @Override
+    public User createUser(User user) {
+        if(userRepository.findByEmail(user.getEmail()).isPresent()) {
+            throw new EntityExistsException("User already exists");
+        }
+
+        User u = new User();
+        u.setName(user.getName());
+        u.setEmail(user.getEmail());
+        u.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+        u.setPhoneNumber(user.getPhoneNumber());
+        u.setRole(user.getRole() != null ? user.getRole() : UserRole.CITIZEN);
+        u.setCreatedAt(LocalDateTime.now());
+        return null;
+    }
+
+    @Override
+    public Optional<User> findById(long id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public User updateUser(Long id, User updatedUser) {
+        User existingUser = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found with id: " + id));
+
+        // Update only the allowed fields
+        existingUser.setName(updatedUser.getName());
+        existingUser.setEmail(updatedUser.getEmail());
+        existingUser.setPhoneNumber(updatedUser.getPhoneNumber());
+
+        // Save updated user
+        return userRepository.save(existingUser);
+
     }
 }
--- engagement-system-be/src/main/java/com/citizen/engagement_system_be/services/UserService.java
+++ engagement-system-be/src/main/java/com/citizen/engagement_system_be/services/UserService.java
@@ -1,8 +1,13 @@
 package com.citizen.engagement_system_be.services;
 
-import com.citizen.engagement_system_be.enums.NotificationType;
+import com.citizen.engagement_system_be.models.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+
+import java.util.Optional;
 
 public interface UserService {
     UserDetailsService userDetailsService();
+    User createUser(User user);
+    Optional<User> findById(long id);
+    User updateUser(Long id,User updatedUser);
 }
